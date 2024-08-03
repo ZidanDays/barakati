defef559d751185cad1e3fa831113aab754b59b9
ZidanDays <zidan.dailer@gmail.com>
2024-08-03 15:49:08 +0800

    index.php

--- index.php
+++ index.php
@@ -82,7 +82,8 @@
                         <h1>Feel the heart beats</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br />tempor
                             incididunt ut labore et dolore magna aliqua.</p>
-                        <a href="https://www.youtube.com/watch?v=K4DyBUG242c" class="play-btn video-popup"><i class="fa fa-play"></i></a>
+                        <a href="https://www.youtube.com/watch?v=K4DyBUG242c" class="play-btn video-popup"><i
+                                class="fa fa-play"></i></a>
                     </div>
                 </div>
             </div>
@@ -195,7 +196,8 @@
             <div class="row">
                 <div class="col-lg-6 p-0">
                     <div class="services__left set-bg" data-setbg="img/services/service-left.jpg">
-                        <a href="https://www.youtube.com/watch?v=JGwWNGJdvx8" class="play-btn video-popup"><i class="fa fa-play"></i></a>
+                        <a href="https://www.youtube.com/watch?v=JGwWNGJdvx8" class="play-btn video-popup"><i
+                                class="fa fa-play"></i></a>
                     </div>
                 </div>
                 <div class="col-lg-6 p-0">
@@ -280,8 +282,7 @@
                                     </div>
                                     <!-- Volume Controls -->
                                     <div class="jp-volume-controls">
-                                        <button class="jp-mute" tabindex="0"><i
-                                                class="fa fa-volume-down"></i></button>
+                                        <button class="jp-mute" tabindex="0"><i class="fa fa-volume-down"></i></button>
                                         <div class="jp-volume-bar">
                                             <div class="jp-volume-bar-value" style="width: 0%;"></div>
                                         </div>
@@ -315,8 +316,7 @@
                                     </div>
                                     <!-- Volume Controls -->
                                     <div class="jp-volume-controls">
-                                        <button class="jp-mute" tabindex="0"><i
-                                                class="fa fa-volume-down"></i></button>
+                                        <button class="jp-mute" tabindex="0"><i class="fa fa-volume-down"></i></button>
                                         <div class="jp-volume-bar">
                                             <div class="jp-volume-bar-value" style="width: 0%;"></div>
                                         </div>
@@ -350,8 +350,7 @@
                                     </div>
                                     <!-- Volume Controls -->
                                     <div class="jp-volume-controls">
-                                        <button class="jp-mute" tabindex="0"><i
-                                                class="fa fa-volume-down"></i></button>
+                                        <button class="jp-mute" tabindex="0"><i class="fa fa-volume-down"></i></button>
                                         <div class="jp-volume-bar">
                                             <div class="jp-volume-bar-value" style="width: 0%;"></div>
                                         </div>
@@ -385,8 +384,7 @@
                                     </div>
                                     <!-- Volume Controls -->
                                     <div class="jp-volume-controls">
-                                        <button class="jp-mute" tabindex="0"><i
-                                                class="fa fa-volume-down"></i></button>
+                                        <button class="jp-mute" tabindex="0"><i class="fa fa-volume-down"></i></button>
                                         <div class="jp-volume-bar">
                                             <div class="jp-volume-bar-value" style="width: 0%;"></div>
                                         </div>
@@ -420,8 +418,7 @@
                                     </div>
                                     <!-- Volume Controls -->
                                     <div class="jp-volume-controls">
-                                        <button class="jp-mute" tabindex="0"><i
-                                                class="fa fa-volume-down"></i></button>
+                                        <button class="jp-mute" tabindex="0"><i class="fa fa-volume-down"></i></button>
                                         <div class="jp-volume-bar">
                                             <div class="jp-volume-bar-value" style="width: 0%;"></div>
                                         </div>
@@ -455,8 +452,7 @@
                                     </div>
                                     <!-- Volume Controls -->
                                     <div class="jp-volume-controls">
-                                        <button class="jp-mute" tabindex="0"><i
-                                                class="fa fa-volume-down"></i></button>
+                                        <button class="jp-mute" tabindex="0"><i class="fa fa-volume-down"></i></button>
                                         <div class="jp-volume-bar">
                                             <div class="jp-volume-bar-value" style="width: 0%;"></div>
                                         </div>
@@ -491,7 +487,8 @@
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="youtube__item">
                         <div class="youtube__item__pic set-bg" data-setbg="img/youtube/youtube-1.jpg">
-                            <a href="https://www.youtube.com/watch?v=yJg-Y5byMMw?autoplay=1" class="play-btn video-popup"><i class="fa fa-play"></i></a>
+                            <a href="https://www.youtube.com/watch?v=yJg-Y5byMMw?autoplay=1"
+                                class="play-btn video-popup"><i class="fa fa-play"></i></a>
                         </div>
                         <div class="youtube__item__text">
                             <h4>David Guetta Miami Ultra Music Festival 2019</h4>
@@ -501,7 +498,8 @@
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="youtube__item">
                         <div class="youtube__item__pic set-bg" data-setbg="img/youtube/youtube-2.jpg">
-                            <a href="https://www.youtube.com/watch?v=K4DyBUG242c?autoplay=1" class="play-btn video-popup"><i class="fa fa-play"></i></a>
+                            <a href="https://www.youtube.com/watch?v=K4DyBUG242c?autoplay=1"
+                                class="play-btn video-popup"><i class="fa fa-play"></i></a>
                         </div>
                         <div class="youtube__item__text">
                             <h4>Martin Garrix (Full live-set) | SLAM!Koningsdag</h4>
@@ -511,7 +509,8 @@
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="youtube__item">
                         <div class="youtube__item__pic set-bg" data-setbg="img/youtube/youtube-3.jpg">
-                            <a href="https://www.youtube.com/watch?v=S19UcWdOA-I?autoplay=1" class="play-btn video-popup"><i class="fa fa-play"></i></a>
+                            <a href="https://www.youtube.com/watch?v=S19UcWdOA-I?autoplay=1"
+                                class="play-btn video-popup"><i class="fa fa-play"></i></a>
                         </div>
                         <div class="youtube__item__text">
                             <h4>Dimitri Vegas, Steve Aoki & Like Mike’s “3 Are Legend”</h4>
@@ -559,55 +558,9 @@
     </section>
     <!-- Countdown Section End -->
 
-    <!-- Footer Section Begin -->
-    <footer class="footer spad set-bg" data-setbg="img/footer-bg.png">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-6">
-                    <div class="footer__address">
-                        <ul>
-                            <li>
-                                <i class="fa fa-phone"></i>
-                                <p>Phone</p>
-                                <h6>1-677-124-44227</h6>
-                            </li>
-                            <li>
-                                <i class="fa fa-envelope"></i>
-                                <p>Email</p>
-                                <h6>DJ.Music@gmail.com</h6>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 offset-lg-1 col-md-6">
-                    <div class="footer__social">
-                        <h2>DJoz</h2>
-                        <div class="footer__social__links">
-                            <a href="#"><i class="fa fa-facebook"></i></a>
-                            <a href="#"><i class="fa fa-twitter"></i></a>
-                            <a href="#"><i class="fa fa-instagram"></i></a>
-                            <a href="#"><i class="fa fa-dribbble"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 offset-lg-1 col-md-6">
-                    <div class="footer__newslatter">
-                        <h4>Stay With me</h4>
-                        <form action="#">
-                            <input type="text" placeholder="Email">
-                            <button type="submit"><i class="fa fa-send-o"></i></button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-			<div class="footer__copyright__text">
-				<p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
-			</div>
-			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        </div>
-    </footer>
-    <!-- Footer Section End -->
+    <?php
+include "footeer.php";
+?>
 
     <!-- Js Plugins -->
     <script src="js/jquery-3.3.1.min.js"></script>
